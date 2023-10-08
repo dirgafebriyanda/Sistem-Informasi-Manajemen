e9a628edd93d487e770eef5056dc1fd7ea2c9748
Dirga Febriyanda <dirgafebriyanda12@gmail.com>
2023-10-08 15:15:21 +0700

    tambah sound dan hilanhkan notif otomatis

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -1,46 +1,72 @@
 @extends('dashboard.layouts.main')
 
 @section('content')
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">My Posts</h1>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">My Posts</h1>
+</div>
+<div class="card">
+    <div class="card-header">
+        <a href="/dashboard">{{ __('Dashboard') }}</a> / Post List
+        <a class="btn btn-sm btn-success float-right" href="/dashboard/posts/create">Add new blog</a>
     </div>
-    @if (session()->has('success'))
-        <div class="alert alert-success col-lg-8" role="alert">
+    <div class="card-body">
+        @if (session()->has('success'))
+        <div class="alert alert-success" id="notif" role="alert">
             {{ session('success') }}
         </div>
-    @endif
-    <a href="/dashboard/posts/create" class="btn btn-sm btn-primary mb-3"> Create new post</a>
-    <div class="table-responsive col-lg-8">
-        <table class="table table-striped table-sm">
-            <thead>
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Title</th>
-                    <th scope="col">Category</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($posts as $post)
+        <audio class="d-none" id="audio" controls autoplay>
+            <source src="{{ asset('sound/notif.wav') }}">
+        </audio>
+        @endif
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr class="bg-dark text-light fw-bold">
+                        <th scope="col">No</th>
+                        <th scope="col">Title</th>
+                        <th scope="col">Category</th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
                         <td>{{ $post->title }}</td>
                         <td>{{ $post->category->name }}</td>
                         <td>
-                            <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span
-                                    data-feather="eye"></span></a>
-                            <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span
-                                    data-feather="edit"></span></a>
+                            <a title="View" href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><span data-feather="eye"></span></a>
+                            <a title="Edit" href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
                             <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
                                 @method('delete')
                                 @csrf
-                                <button class="badge bg-danger border-0" onclick="return confirm('Are your sure?')"><span
-                                        data-feather="x-circle"></span></button>
+                                <button title="Delete" class="badge bg-danger border-0" onclick="return confirm('Are your sure?')"><span data-feather="x-circle"></span></button>
                             </form>
                         </td>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+</div>
+
+{{-- <script>
+        var notif = document.getElementById('notif');
+        var audio = document.getElementById('audio');
+        function playNotif(){
+            audio.play();
+            notif.style.display = 'block';
+        }
+        window.onload = playNotif;
+    </script> --}}
+<script>
+    function hideAlert() {
+        $("#notif").slideUp(500,
+            function() {
+                $(this).remove();
+            });
+    }
+    setTimeout(hideAlert, 3000);
+</script>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WPU Blog | {{ $title }}</title>
-    <link href="{{ asset('dist/css/bootstrap.min.css') }}" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="{{ asset('dist/css/bootstrap.min.css') }}" rel="stylesheet">
+       
     <link rel="stylesheet" href="{{ asset('dist/fontawesome-free/css/all.min.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
@@ -19,8 +19,7 @@
         @yield('content')
     </div>
 
-    <script src="{{ asset('dist/js/bootstrap.bundle.min.js') }}"
-        integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+    <script src="{{ asset('dist/js/bootstrap.bundle.min.js') }}">
     </script>
 </body>
 
